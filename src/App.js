0a07a82f3d545c7b28ce8f8f5b665f1e78b937c5
bluezipGC <clareg1@mymail.nku.edu>
2023-03-25 14:36:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey, it sure friggin does!
         </p>
         <a
           className="App-link"
